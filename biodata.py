c277a463879ebcfd68d4fd79349ddf16084edc33
ImanRiaz <mani7889khan@gmail.com>
2025-07-22 23:59:18 +0500

    Input

---
+++ biodata.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+fname=input("Enter your father's name: ")
+
+print("Your name is ",name)
+print("Your Father's name is ",fname)
+print("Your age is ",age)
