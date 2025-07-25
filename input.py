0f00b3eee606d4a555faf811f1789dc9db6a1711
ImanRiaz <mani7889khan@gmail.com>
2025-07-25 23:33:45 +0500

    Input

---
+++ input.py
@@ -0,0 +1,4 @@
+firstnumber=input("Enter yor first number: ")
+second=input("Enter your second number: ")
+sum=int(firstnumber)+int(second)
+print("The sum of ",firstnumber," and ",second," is",sum,".")
