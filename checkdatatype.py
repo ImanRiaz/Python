82ad37ed4852935d562f5ae20d918e495a71cf2c
ImanRiaz <mani7889khan@gmail.com>
2025-07-26 21:32:17 +0500

    Practice

---
+++ checkdatatype.py
@@ -0,0 +1,12 @@
+a="IMAN"
+b=65
+c=98.09
+d=[1,2,3]
+e=True
+
+
+print("Type of value 1 is",type(a))
+print("Type of value 2 is",type(b))
+print("Type of value 3 is",type(c))
+print("Type of value 4 is",type(d))
+print("Type of value 5 is",type(e))
