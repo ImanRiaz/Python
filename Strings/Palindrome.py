901580b07d952ad3e1f19cb14dd934fa6b506fba
ImanRiaz <mani7889khan@gmail.com>
2025-07-30 15:52:28 +0500

    Upgraded

---
+++ Strings/Palindrome.py
@@ -0,0 +1,10 @@
+word = "mada"
+
+reversed_word = ""
+for char in word:
+    reversed_word = char + reversed_word
+
+if word == reversed_word:
+    print("Yes, it's a palindrome!")
+else:
+    print("No, it's not a palindrome.")
