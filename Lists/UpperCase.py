6782e6f60f7b02d684b079d942230ef00ed4a1dd
ImanRiaz <mani7889khan@gmail.com>
2025-08-04 13:50:05 +0500

    Practice

---
+++ Lists/UpperCase.py
@@ -0,0 +1,11 @@
+words = ["ok", "word", "run", "fine", "apple"]
+
+for i in range(len(words)):
+    words[i] = words[i].upper()
+
+print(words)
+
+for i in range(len(words)):
+    words[i]=words[i].lower()
+
+print(words)
