9f42e79a436755eaacaba8672f779b999bf4cddd
ImanRiaz <mani7889khan@gmail.com>
2025-08-10 17:09:59 +0500

    practice

---
+++ Tuples&Set/checknumber.py
@@ -0,0 +1,14 @@
+number=(12,34,56,2,23)
+
+if 12 in number:
+    print("12 is present in tuple")
+else:
+    print("12 is not present in tuple")
+
+
+if 10 in number:
+    print("10 is present in tuple")
+else:
+    print("10 is not present in tuple")
+
+print(number)
